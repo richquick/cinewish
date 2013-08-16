7d4e67ea176d2d023c9da2291c9263a396c5321b
Rich Quick <rich@speaktorich.com>
2013-08-16 14:36:06 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cinewish::Application.config.secret_key_base = 'c45dd73fbabcbc6e0c7436cd3caee3f1fd67119d654c27a1d0cad0cdce016a8780de3c18402556e5da5e121d41e9e56374c8c13e825f1b4b5f2296151ff534c6'
